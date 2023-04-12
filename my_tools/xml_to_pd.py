e8fab0dfde7fe90b363314b8c6e8216d89d0e35b
dnstels <35730176+dnstels@users.noreply.github.com>
2023-04-12 18:34:21 +0400

    Update xml_to_pd.py

--- my_tools/xml_to_pd.py
+++ my_tools/xml_to_pd.py
@@ -17,9 +17,12 @@ Created on Tue Apr 11 21:20:58 2023
 # root_tag = root.documentElement.tagName
 # =============================================================================
 
+
 from xml.etree import ElementTree as ET
 import sys
 import pandas as pd
+
+import console_process_bar1 as bar
 
 xml_test_str2=u'''
 <CAT>
@@ -33,31 +36,75 @@ xml_test_str2=u'''
 </CAT>
 '''
 
-et = ET.parse('Data.xml')
+et = ET.parse('test_exp.xml')
 
-et = ET.fromstring(xml_test_str2)
+#et = ET.fromstring(xml_test_str2)
 
 if type(et) is ET.ElementTree:
     root = et.getroot()
 else:
     root=et
 
-path = [root.tag]
-outDataSet = pd.DataFrame({'Path':[tuple(path)],'Text':root.text, 'Attr': [root.attrib]})
-print(type(root.attrib))
+#print(type(root.attrib))
+from lxml import etree
+doc = etree.parse("test_exp.xml")
+root = doc.getroot()
 
-def xmlTreeToDataFrame(tree, path):
+# =============================================================================
+# nodes = root.xpath(".//*")
+# Total=len(nodes)
+# for node in nodes:
+#     print(node.tag)
+# =============================================================================
+
+def xmlTreeToDataFrame(tree, path, index=0, total=0):
     global outDataSet
+    if index==0 :
+        bar.printProgressBar(index, total,  prefix = 'Progress:', suffix = 'Complete', length = 50)
     for child in tree:
+        index=index+1
         pathA = path.copy()
         pathA.append(child.tag)
+        bar.printProgressBar(index, total,  prefix = 'Progress ('+str(index)+') :', suffix = 'Complete', length = 50)
         attr=[child.attrib]
-        outDataSet = pd.concat([outDataSet, pd.DataFrame({'Path':[tuple(pathA)], 'Text':child.text, 'Attr':attr})], axis=0, ignore_index=True)
-        if len(child)>0:
-            xmlTreeToDataFrame(child, pathA)
+        row=pd.DataFrame({'Path':[tuple(pathA)], 'Text':child.text, 'Child_count':len(child), 'Attr':attr})
+        #print(row)
+        outDataSet = pd.concat([outDataSet, row], axis=0, ignore_index=True)
+        if len(child)>0 and index<100:
+            len(child)
+            xmlTreeToDataFrame(child, pathA, index, total)
+            pass
 
-xmlTreeToDataFrame(root, path)
-#print(outDataSet)
 
+path = [root.tag]
+outDataSet = pd.DataFrame({'Path':[tuple(path)],'Text':root.text, 'Child_count':len(root), 'Attr': [root.attrib]})
+# =============================================================================
+#
+# xmlTreeToDataFrame(root, path,0, len(root.xpath(".//*")))
+# print(outDataSet)
+#
+# var1=outDataSet.groupby('Path')["Path"].count()
+# print(var1)
+#
+# =============================================================================
+#print(root.xpath('/'+root.tag))
+
+tree = etree.ElementTree(root)
+arr=list(root.xpath(".//*"))#[:10000]
+total=len(root.xpath(".//*"))
+total=len(arr)
+#for i,e in enumerate(root.iter()[:1000]):
+for i,e in enumerate(arr):
+    #print(i,tree.getpath(e), e.tag)
+    bar.printProgressBar(i, total,  prefix = f'Progress ({i}/{total}) :', suffix = 'Complete', length = 50)
+    attr=[e.attrib]
+    row=pd.DataFrame({'Path':tree.getpath(e), 'Text':e.text, 'Child_count':len(e), 'Attr':attr})
+    outDataSet = pd.concat([outDataSet, row], axis=0, ignore_index=True)
+
+print(outDataSet)
 var1=outDataSet.groupby('Path')["Path"].count()
 print(var1)
+
+
+
+
